7edc45a1956f597a155f96101ac6b3a245f91a5e
josesalgado1024 <tierengelmsk@gmail.com>
2016-06-09 17:32:34 -0300

    .

--- glacier_lite.py
+++ glacier_lite.py
@@ -1,32 +1,80 @@
+#!/usr/bin/python
+# encoding: utf-8
+"""
+        .-.
+       /_ _\
+       |o^o|
+       \ _ /
+      .-'-'-.
+    /`)  .  (`\
+   / /|.-'-.|\ \
+   \ \| (_) |/ /  .-""-.
+    \_\'-.-'/_/  /[] _ _\
+    /_/ \_/ \_\ _|_o_LII|_
+      |'._.'|  / | ==== | \
+      |  |  |  |_| ==== |_|
+       \_|_/    ||" ||  ||
+       |-|-|    ||LI  o ||
+   jgs |_|_|    ||'----'||
+      /_/ \_\  /__|    |__\
+"""
+
 from libs.glacier import Glacier
 from libs.configs import  my_conf
+import argparse
+import os
+import csv
 
-"""
-    def __init__(self,
-                access_key_id=None,
-                secret_access_key=None,
-                region_name='us-east-1',
-                bucket=None,
-                filename=None,
-                restore_for=5):
-"""
-
-
+def get_args():
+    """Funcion encargada de cargar los argumentos requeridos para el script."""
+    parser = argparse.ArgumentParser(
+        description='Script para bajar material de GLACIER')
+    parser.add_argument('-f',
+                        '--datafile',
+                        type=str,
+                        help=' Archivo csv que contiene lista de archivos a descargar...',
+                        required=True)
+    parser.add_argument('-l',
+                        '--log',
+                        type=str,
+                        help=' Archivo csv que contiene lista de archivos a descargar...',
+                        required=False)
+    args = parser.parse_args()
+    datafile = args.datafile
+    log = args.log
+    return datafile, log
 
 
 def main():
+    datafile, log = get_args()
+
     try:
         glacier = Glacier(bucket=my_conf.BUCKET_NAME,
                           access_key_id=my_conf.ACCESS_KEY_ID,
                           secret_access_key=my_conf.SECRET_ACCESS_KEY,
                           region_name=my_conf.REGION,
-                          name_regex=my_conf.NAME_REGEX)
-        glacier.restore_list_of_files(['sepa_2_comercio-sepa-9_2016-05-05_01-01-05.zip'])
+                          name_regex=my_conf.NAME_REGEX,
+                          logs=True)
     except Exception as e:
         print e
+        exit(1)
 
-    # galcier.retrieve('sepa_1_comercio-sepa-10_2016-04-22_03-46-32.zip', True)
-
+    if not os.path.exists(datafile):
+        print 'ERROR: El Archivo: {f} no existe...'.format(f=datafile)
+        exit(1)
+    data = csv.DictReader(open(datafile))
+    to_download = []
+    for line in data:
+        storage_class = line['storage_class'].replace(' ', '')
+        filename = line['filename'].replace(' ', '')
+        if storage_class == 'GLACIER':
+            print 'file: \"{f}\" Added to queue , (storage_class=\"{c}\")'.format(f=filename,
+                                                                                  c=storage_class)
+            to_download.append(filename)
+        else:
+            print 'file: \"{f}\" Skyped, storage_class=\"{c}\"'.format(f=filename,
+                                                                       c=storage_class)
+    glacier.restore_list_of_files(to_download)
 
 
 
--- libs/glacier.py
+++ libs/glacier.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python
 # encoding: utf-8
+
+
 import boto
 import re
 from errHandling import *
 
-
-
-
-
 class Glacier(object):
     """
-    UNA CLASE.
+    UNA CLASE...
+    TODO.
     """
     def __init__(self,
                 access_key_id=None,
@@ -18,10 +17,12 @@ class Glacier(object):
                 region_name='us-east-1',
                 bucket=None,
                 name_regex=None,
-                restore_for=5):
+                restore_for=5,
+                logs=False):
         """
         TODO
         """
+        self.logs_enabled = logs
         if secret_access_key == None or secret_access_key == None or region_name == None:
             raise BadAuthData()
         else:
@@ -58,38 +59,44 @@ class Glacier(object):
         except Exception as e:
             raise BucketNotExists
 
+    def _log(self, msg, error=False):
+        if self.logs_enabled:
+            print '{tE}: {m}'.format(m=msg, tE= 'ERROR' if error else 'INFO')
+
     def restore_file(self, filename, days=None):
-        """Comentario."""
+        """TODO"""
         d = self._RESTORE_FOR if days == None else days
         if re.search(self._NAME_REGEX, filename) == None:
-            print '\"filename\", mal formado. [{f}]'.format(f=filename)
+            self._log('\"filename\", mal formado. [{f}]'.format(f=filename), True)
             return False
         try:
             key = self.selected_bucket.get_key(filename)
         except Exception:
-            print 'No existe el archivo: {f}'.format(f=filename)
+            self._log('No existe el archivo: {f}'.format(f=filename), True)
             return False
         try:
             key.restore(days=d)
         except Exception:
+            self._log('La clase de \"{f}\" no es \"GLACIER\"'.format(f=filename), True)
             return False
         return True
 
-    def restore_list_of_files(self, list_of_files=None):
+    def restore_list_of_files(self, list_of_files=None, dowload=False):
+        """TODO."""
         if list_of_files==None or not type(list_of_files) is list:
             raise BadFileListToRestore
         else:
             for filename in list_of_files:
                 if self.restore_file(filename):
                     if self.is_available(filename):
-                        print 'Esta disponible el archivo {f}, puede ser descargado..'.format(f=filename)
+                        self._log('Esta disponible el archivo \"{f}\" y puede ser descargado..'.format(f=filename))
                     else:
-                        print 'El archivo {f}, aun no puede ser descargado..'.format(f=filename)
+                        self._log('El archivo {f}, aun no puede ser descargado..'.format(f=filename))
                 else:
-                    print 'Imposible restaurar: {f}'.format(f=filename)
+                    self._log('Imposible restaurar: \"{f}\".'.format(f=filename), True)
         return True
 
     def is_available(self, filename):
-        """Comentario."""
+        """TODO."""
         key = self.selected_bucket.get_key(filename)
         return key.ongoing_restore
